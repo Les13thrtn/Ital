82839eeeadea315b0e704304517227df9f37ed0e
Les13thrtn <94843265+Les13thrtn@users.noreply.github.com>
2022-02-07 17:36:18 +0200

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+// wrapper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+});
